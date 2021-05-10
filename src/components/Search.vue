3df2c76c11e54a12b6500689f669d0eea0959a7e
MirkoGriffo <mirkogriffo@yahoo.it>
2021-05-10 23:29:58 +0200

    Add searchbar

---
+++ src/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <div><input type="text" v-model="searchText" /></div>
+    <div class="btn">
+      <button type="submit" @click.prevent="$emit('performSearch', searchText)">
+        Cerca
+      </button>
+    </div>
+    <div class="btn">
+      <button type="reset" @click="resetSearch">Reset</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+  methods: {
+    resetSearch() {
+      this.searchText = " ";
+      $emit("performSearch", searchText);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
